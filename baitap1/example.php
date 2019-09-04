6e4ae8c6ad7d34027d7bdeeada411fe69b4b0411
anngocle <52774371+anngocle@users.noreply.github.com>
2019-09-04 09:49:41 +0700

    php-v1.0

---
+++ baitap1/example.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo "Today is " . date("Y/m/d") . "<br>";
+        // create months array()
+$months = array (1 => 'January','February', 'March', 'April', 'May','June', 'July', 'August', 'September','October', 'November', 'December');
+//Make the days and years arrays:
+$days = range(1,31);
+$years = range(2011,2031);
+// Make the months pull-down menu:
+echo '<select name="month">';
+foreach ($months as $key =>$value){
+    echo "<option value=\"$key\">$value</option>\n";
+}
+echo '</select>';
+// Make the days pull-down menu:
+echo '<select name="day">';
+ foreach ($days as $value) {
+ echo "<option value=\"$value\">$value</option>\n";
+ }
+ echo '</select>';
+   // Make the years pull-down menu:
+	 echo '<select name="year">';
+         foreach ($years as $value) {
+ echo "<option value=\"$value\">$value</option>\n";
+ }
+echo '</select>';      
+   
+// Create one array:
+ $mexico = array('YU' => 'Yucatan','BC' => 'Baja California','OA' => 'Oaxaca');
+ // Create another array:
+$us = array ('MD' => 'Maryland','IL' => 'Illinois','PA' => 'Pennsylvania','IA' => 'Iowa');
+// Combine the arrays:
+$n_america = array('Mexico' => $mexico,'United States' => $us);
+// Loop through the countries:
+foreach ($n_america as $country =>$list) {
+	// Print a heading:
+	echo "<h2>$country</h2><ul>";
+	 // Print each state, province, or territory:
+foreach ($list as $k => $v) {
+	echo "<li>$k - $v</li>\n";
+	 	 }
+	 	 // Close the list:
+	 	 echo '</ul>';
+	
+	 } // End of main FOREACH
+ $name = array('an','ban','long');
+ sort($name);
+ $dem =count($name);
+ echo $dem;
+ foreach ($name as $value){
+     echo "$value <br>";
+ }
+ 
+ 
+        ?>
+    </body>
+</html>
---
+++ baitap1/includes/header1.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Chào mừng đến trang quản trị OPX MCU </title> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title>emxple php</title>
+         <style>
+body {
+  margin: 0;
+}
+
+/* Style the header */
+.header {
+  background-color: #f1f1f1;
+  padding: 20px;
+  text-align: center;
+}
+
+/* Style the top navigation bar */
+.topnav {
+  overflow: hidden;
+  background-color: #6a5acd;
+}
+
+/* Style the topnav links */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change color on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Style the footer */
+/* Footer */
+.footer {
+  padding: 20px;
+  text-align: center;
+  background: #ddd;
+  margin-top: 20
+  
+}
+</style>
+    </head>
+    <body>
+        <div class="header">
+	 	 	 <h1>Your Website</h1>
+	 	 	 <h2>catchy slogan...</h2>
+     </div>
+     <div class="topnav">
+  <a href="index1.php">Home Page</a>
+  <a href="Calculator.php">Calculator</a>
+  <a href="Register.php">Register</a>
+  <a href="View_Users.php">View Users</a>
+  <a href ="#">Other</a>
+	 	 	 
+      </div>
+	 	 <div id="content"><!-- Start of the page-specific content. -->
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
