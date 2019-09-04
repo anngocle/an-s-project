a78bf0b3a22cc2a20cacd2f6c2c097d6cba37624
anngocle <52774371+anngocle@users.noreply.github.com>
2019-09-04 16:42:12 +0700

    Create calendar.php

---
+++ baitap1/calendar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>calendar</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        // Make the months array:
+$months = array (1 => 'January','February', 'March', 'April', 'May','June', 'July', 'August', 'September','October', 'November', 'December');
+// Make the months pull-down menu:
+ echo '<select name="month">';
+        ?>
+    </body>
+</html>
